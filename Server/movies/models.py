ee2a15917254ef4313f77857518aec86b5ccb863
seungcheol-py <seung941226@gmail.com>
2021-05-20 16:57:21 +0900

    20210520/vue django/ADD client AND Server

--- Server/movies/models.py
+++ Server/movies/models.py
@@ -11,6 +11,29 @@ class Movie(models.Model):
     overview = models.TextField()
     poster_path = models.TextField(null=True)
     vote_average = models.DecimalField(max_digits=4, decimal_places=2)
+    # upcoming 같은 것을 저장할 필드를 추가할 예정이다.
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
---
+++ client/src/components/MoviesList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col">
+    <div class="card h-100">
+      <img :src="moviePosterPath" class="card-img-top" alt="movie">
+      <div class="card-body">
+        <h5 class="card-title">{{ movie.title }}</h5>
+        <div>{{ movie.overview }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'MoviesList',
+  props: {
+    movie: {
+      type: Object
+    }
+  },
+  computed: {
+    moviePosterPath: function () {
+      return `https://image.tmdb.org/t/p/w500${this.movie.poster_path}`
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
