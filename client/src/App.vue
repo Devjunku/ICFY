1d095fc9602efc58146c975c4bebd96d401e7afc
seungcheol-py <seung941226@gmail.com>
2021-05-21 09:24:46 +0900

    210521/front/login.vue signup.vue 로그인 관련 기능 프론트단 구현

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,65 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Movies</router-link>
-    </div>
-    <router-view/>
+    <nav class="navbar navbar-expand fixed-top navbar-dark bg-dark d-flex justify-content-between">
+      <div class="ms-3">
+        <router-link :to="{ name: 'MovieHome'}" class="navbar-brand">
+          <img src="./assets/logo.png" alt="logo image" height="35">
+        </router-link>  
+      </div>
+      <div>
+        <input type="text" size=35 class="ms-3" v-model="searchKeyword" @keyup.enter="searchEnter" placeholder="영화 제목을 검색하세요">
+      </div>
+      <div>
+        <ul v-if="isLogin" class="navbar-nav">
+          <li class="nav-item mx-2">
+            <router-link :to="{ name: 'MyProfile'}" class="nav-link text-decoration-none text-light fw-bold">내 프로필</router-link>
+          </li>
+          <li class="nav-item mx-2">
+            <router-link @click.native="logout" to="#" class="nav-link text-decoration-none text-light fw-bold">로그아웃</router-link>
+          </li>
+        </ul>
+        <ul v-else class="navbar-nav">
+          <li class="nav-item mx-2">
+            <router-link :to="{ name: 'Signup'}" class="nav-link text-decoration-none text-light fw-bold">회원가입</router-link>
+          </li>
+          <li class="nav-item mx-2">
+            <router-link :to="{ name: 'Login'}" class="nav-link text-decoration-none text-light fw-bold">로그인</router-link>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+
+    <router-view @login="isLogin = true"/>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      searchKeyword: null,
+       isLogin: false,
+    } 
+  },
+  methods: {
+    logout: function () {
+      this.isLogin = false
+      localStorage.removeItem('jwt')
+      this.$router.push({ name: 'MovieHome' })
+    }
+  },
+  created: function () {
+    const token = localStorage.getItem('jwt')
+    if (token) {
+      this.isLogin = true
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
