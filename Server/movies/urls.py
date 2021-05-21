c2c0d1812ff20e0d18cbf7f5adcb8060639d8498
박준규 <junku8782@gmail.com>
2021-05-21 13:23:49 +0900

    20210521/django/movies/urls.py, views.py

--- Server/movies/urls.py
+++ Server/movies/urls.py
@@ -4,14 +4,15 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
     # 메인 페이지 (여러 가지 추천 영화가 나온다.)
-    path('', views.movie_index, name='movie_index'),
+    path('', views.movie_index),
     # 단일 영화 페이지
-    path('<int:movie_id>/', views.movie_detail, name='movie_detail'),
+    path('<int:movie_id>/', views.movie_detail),
     # GET : 특정 영화에 대한 커뮤니티
+    path('<int:movie_id>/community/', views.community),
     # POST: 특정 영화에 대한 리뷰 생성
-    path('<int:movie_id>/community', views.community, name='community'),
+    path('movies/<int:movie_pk>/reviews/', views.review_create),
     # GET : 특정 리뷰 보기
     # PUT : 특정 리뷰 수정
     # DELETE : 특정 리뷰 삭제 
-    # path('community/<int:review_pk>', views.review, name='review'),
+    path('community/<int:review_pk>', views.review_detail),
 ]
--- Server/movies/views.py
+++ Server/movies/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import JsonResponse, HttpResponse
 from .models import Movie, Review
 from .serializers import MovieListSerializer, ReviewListSerializer
+from Server.movies import serializers
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -29,21 +30,45 @@ def movie_detail(request, movie_id):
     return Response(serializer.data)
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def community(request, movie_id):
     # GET 방식이면 리뷰를 보는 행위
     if request.method == 'GET':
         movie = get_object_or_404(Movie, id=movie_id)
-        print(movie)
         reviews = movie.review_set.all()
         serializer = ReviewListSerializer(reviews, many=True)
         return Response(serializer.data)
 
-    # POST 방식이면 리뷰를 쓰는 행위
-    elif request.method == 'POST':
-        movie = get_object_or_404(Movie, id=movie_id)
-        serializer = ReviewListSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(movie=movie)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+@api_view(['POST'])
+def review_create(request, movie_pk):
+    movie = get_object_or_404(Movie, id=movie_pk)
+    serializers = ReviewListSerializer(data=request.data)
+    if serializers.is_valie(raise_exception = True):
+        serializers.save(movie=movie)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def review_detail(request, review_pk):
+
+    # GET 방식이면, detail 페이지 데이터 전송
+    review = get_object_or_404(Review, pk=review_pk)
+    if request.method == 'GET':
+        serializer = ReviewListSerializer(review)
+        return Response(serializer.data)
+
+    # DELETE 방식이면 리뷰를 작성 데이터를 DB에 삭제 후 전송
+    elif request.method == 'DELETE':
+        data = {
+            'Delete': f'{request.user}님이 작성하신 영화 리뷰 {review.title}이 삭제되었습니다.'
+        }
+        review.delete()
+        return Response(data, status=status.HTTP_204_NO_CONTENT)
+
+    # DELETE 방식이면 리뷰를 작성 데이터를 DB에 수정 후 전송
+    elif request.method == 'PUT':
+        serializer = ReviewListSerializer(Review, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
