65743bedff7521c508825ee063a4885bc09912d4
seungcheol-py <seung941226@gmail.com>
2021-05-24 09:44:01 +0900

    210524/front/Signup.vue added password check

--- client/src/views/accounts/Signup.vue
+++ client/src/views/accounts/Signup.vue
@@ -7,6 +7,7 @@
           <input type="text" class="form-control" id="username" v-model="credentials.username" placeholder="아이디를 입력하세요">
       </div>
     </div>
+      <!-- <button class="btn btn-success mb-3" @click="checkId">아이디 중복 체크하기</button> -->
     <div class="input-group justify-content-center">
       <div class="mb-3">
         <input type="password" class="form-control" id="password" v-model="credentials.password" placeholder="비밀번호를 입력하세요">
@@ -17,8 +18,9 @@
         <input type="password" class="form-control" id="passwordConfirmation" v-model="credentials.passwordConfirmation" placeholder="비밀번호를 재입력하세요">
       </div>
     </div>
+    <div v-if="different" class="text-danger mt-3">두 비밀번호가 일치하지 않습니다.</div>
     <div class="mt-3">
-      <button class="btn btn-warning" @click="signup(credentials)">회원가입</button>
+      <button class="btn btn-warning" @click="signup">회원가입</button>
     </div>
   </div>
 </template>
@@ -34,24 +36,46 @@ export default {
         username: null,
         password: null,
         passwordConfirmation: null,
-      }
+      },
+      different: false
     }
   },
   methods: {
     signup: function () {
-      axios({
-        method: 'post',
-        url: 'http://127.0.0.1:8000/accounts/signup/',
-        data: this.credentials,
-      })
-        .then(res => {
-          console.log(res)
-          this.$router.push({ name: 'Login' })
+      if (this.credentials.password === this.credentials.passwordConfirmation) {
+        console.log(1)
+        axios({
+          method: 'post',
+          url: 'http://127.0.0.1:8000/accounts/signup/',
+          data: this.credentials,
         })
-        .catch(err => {
-          console.log(err)
-        })
-    }
+          .then(res => {
+            console.log(res)
+            this.$router.push({ name: 'Login' })
+          })
+          .catch(err => {
+            console.log(err)
+          })
+        this.different = false
+      }
+      else {
+        this.different = true
+      }
+    },
+    // checkId: function () {
+    //   axios({
+    //     method: 'post',
+    //     url: 'http://127.0.0.1:8000/accounts/check/',
+    //     data: this.credentials,
+    //   })
+    //     .then(res => {
+    //       console.log(res)
+    //       this.$router.push({ name: 'Login' })
+    //     })
+    //     .catch(err => {
+    //       console.log(err)
+    //     })
+    // }
   }
 }
 </script>
